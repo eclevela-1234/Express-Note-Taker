65f9f1aed391a4c42ff758f6376de3c1a4921cf0
Eliot Cleveland <eclevela@richmond.edu>
2022-08-08 21:45:08 -0400

    server working

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const data = require('./db/db.json');
+
+app.get('/api/notes', (req, res) => {
+    res.json(data);
+});
+
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
