eac9e96366503c2ebea3480daeea5b929aa814cc
Eliot Cleveland <eclevela@richmond.edu>
2022-08-08 23:05:38 -0400

    stylsheet served

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path")
 const express = require('express');
 const app = express();
 const data = require('./db/db.json');
@@ -6,7 +8,14 @@ app.get('/api/notes', (req, res) => {
     res.json(data);
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+});
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+});
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
